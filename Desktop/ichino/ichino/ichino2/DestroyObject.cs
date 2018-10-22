f1cb6e137925eb9d51ac3130a6fe3c9143991bea
RenMitsumata <vqyw8ade4rrkjh8y4zjb@docomo.ne.jp>
2018-10-22 11:01:22 +0900

    first commit

---
+++ Desktop/ichino/ichino/ichino2/DestroyObject.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour
+{
+
+    // ★追加
+    // エフェクトプレハブのデータを入れるための箱を作る。
+    public GameObject effectPrefab;
+
+    private GameObject Root;
+
+    State state;
+    private void Start()
+    {
+        Root = this.transform.root.gameObject;
+        state = Root.GetComponent<State>();
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("Shell"))
+        {
+            state.Damage(10);
+            if (state.getHP() <= 0)
+            {
+                Destroy(this.gameObject);
+                // ★追加
+                // エフェクトを実体化（インスタンス化）する。
+                GameObject effect = (GameObject)Instantiate(effectPrefab, transform.position, Quaternion.identity);
+
+                // ★追加
+                // エフェクトを２秒後に画面から消す
+                Destroy(effect, 2.0f);
+            }
+            
+            Destroy(other.gameObject);
+
+            
+        }
+    }
+}
